7aa8384fc35c150d641ec01318cbb02388777318
Carter Pape <carterpape@gmail.com>
2020-01-27 22:36:27 -0700

    makes setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# # # # # # # # # # # # # # # # # # # #
+# NewBot, a journalism tool
+# Copyright 2020 Carter Pape
+# 
+# See file LICENSE for licensing terms.
+# # # # # # # # # # # # # # # # # # # #
+
+import setuptools
+
+setuptools.setup(
+    name        = 'NewsBot',
+    version     = '0.0.0',
+    packages    = setuptools.find_packages(),
+)
