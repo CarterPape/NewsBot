06ffa3a1b75e6a25f09677d0eb1a11fdbb3c4a18
Carter Pape <carterpape@gmail.com>
2020-02-12 20:50:53 -0700

    properly loads spider classes

--- __main__.py
+++ __main__.py
@@ -11,12 +11,20 @@ import twisted.internet.reactor
 import scrapy.utils.log
 import scrapy.utils.project
 import os
+import scrapy.spiderloader
 
 os.environ["SCRAPY_SETTINGS_MODULE"] = "NewsBot.settings"
-scrapy.utils.log.configure_logging(settings = scrapy.utils.project.get_project_settings())
+project_settings = scrapy.utils.project.get_project_settings()
+scrapy.utils.log.configure_logging(settings = project_settings)
+
+spider_loader = scrapy.spiderloader.SpiderLoader(project_settings)
+spider_classes = [
+    spider_loader.load(spider_name)
+    for spider_name in spider_loader.list()
+]
 
 registry = newsbot_tasking.job_registry.NewsBotJobRegistry(
-    from_spider_classes = NewsBot.spiders.self_scheduling_spider.SelfScheduling.__subclasses__(),
+    from_spider_classes = spider_classes,
 )
 
 registry.schedule_all_jobs()
