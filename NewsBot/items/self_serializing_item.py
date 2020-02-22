78e8985ceebb400c151437834e892b3ed1ccdf7a
Carter Pape <carterpape@gmail.com>
2020-02-22 00:42:01 -0700

    removes serialization hack

--- NewsBot/items/self_serializing_item.py
+++ NewsBot/items/self_serializing_item.py
@@ -10,32 +10,21 @@ import json
 
 
 class SelfSerializingItem(scrapy.Item):
-    _serialized = scrapy.Field(ignore_when_serializing = True)
-    
     def serialized(self) -> dict:
-        if "_serialized" in self:
-            pass
-        else:
-            simple_self = dict()
-            
-            for field_key, field_properties in self.fields.items():
-                if (
-                    "ignore_when_serializing" in field_properties
-                ) and (
-                    field_properties["ignore_when_serializing"]
-                ):
-                    continue
-                
-                else:
-                    try:
-                        if hasattr(field_properties, "serializer"):
-                            simple_self[field_key] = field_properties["serializer"](self[field_key])
-                        else:
-                            simple_self[field_key] = self[field_key]
-                    
-                    except KeyError:
-                        continue
-            
-            self["_serialized"] = json.dumps(simple_self, sort_keys = True)
+        simple_self = dict()
         
-        return self["_serialized"]
+        for field_key, field_properties in self.fields.items():
+            if (
+                "ignore_when_serializing" in field_properties
+            ) and (
+                field_properties["ignore_when_serializing"]
+            ):
+                continue
+            
+            else:
+                if hasattr(field_properties, "serializer"):
+                    simple_self[field_key] = field_properties["serializer"](self[field_key])
+                else:
+                    simple_self[field_key] = self[field_key]
+        
+        return json.dumps(simple_self, sort_keys = True)
