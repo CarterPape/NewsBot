9d96a75797d65afb9590eb478aed0a3d9644efe7
Carter Pape <public@carterpape.com>
2021-02-08 00:24:41 -0700

    puts placeholder

---
+++ newsbot/spiders/power_outage_spider.py
@@ -0,0 +1,8 @@
+"for simple list"
+"https://www.rockymountainpower.net/etc/pcorp/datafiles/outagemap/listUT.json"
+
+"for detail list"
+"https://www.rockymountainpower.net/etc/pcorp/datafiles/outagemap/mapUT.json"
+
+
+"typically refreshed every 15 minutes"
