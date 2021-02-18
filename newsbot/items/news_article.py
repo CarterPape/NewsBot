bc39aff50de9b712dcc3e708d8e7c26ac5cf5ad5
Carter Pape <public@carterpape.com>
2021-02-18 16:10:11 -0700

    defines news_article emails

--- newsbot/items/news_article.py
+++ newsbot/items/news_article.py
@@ -20,14 +20,16 @@ class NewsArticle(emailable_item.EmailableItem):
     news_source =   scrapy.Field(ignore_when_serializing = True)
     
     def synthesize_email_subject(self) -> str:
-        return f"{self['news_source']['name']}: {self['title']}"
+        return f"{self['news_source'].name}: {self['title']}"
     
     def synthesize_html_email_body(self) -> str:
         return self._get_email_template().safe_substitute({
-            "email_subject":        self.synthesize_email_subject(),
-            "news_source_name":     self["news_source"]["name"],
-            "article_title":        self["title"],
-            "article_description":  self["description"],
-            "article_url":          self["clean_url"],
-            "article_domain":       urllib.parse.urlparse(self["clean_url"]).netloc,
+            "email_subject":            self.synthesize_email_subject(),
+            "news_source_name":         self["news_source"].name,
+            "news_source_home_url":     self["news_source"].home_url,
+            "news_source_search_url":   self["news_source"].search_url,
+            "article_title":            self["title"],
+            "article_description":      self["description"],
+            "article_url":              self["clean_url"],
+            "article_domain":           urllib.parse.urlparse(self["clean_url"]).netloc,
         })
