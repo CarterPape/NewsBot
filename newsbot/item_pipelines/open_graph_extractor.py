07bced535b05ddd73ddcab0193b70c2c5ea89bf7
Carter Pape <public@carterpape.com>
2021-02-18 16:09:35 -0700

    implements og:tag scraping

--- newsbot/item_pipelines/open_graph_extractor.py
+++ newsbot/item_pipelines/open_graph_extractor.py
@@ -9,11 +9,20 @@ import logging
 import scrapy
 import newsbot.items.news_article as news_article
 import newsbot.item_pipelines.item_pipeline as item_pipeline
+import metadata_parser
 
 class OpenGraphExtractor(item_pipeline.ItemPipeline):
     def process_item(self,
         item:   news_article.NewsArticle,
         spider: scrapy.spiders.Spider,
     ) -> news_article.NewsArticle:
-        print(item['clean_url'])
-        raise scrapy.exceptions.DropItem("Skipping for now")
+        
+        page_data = metadata_parser.MetadataParser(
+            url = item["clean_url"],
+            search_head_only = True,
+        )
+        item["title"] =        page_data.get_metadatas("title")[0]
+        item["description"] =  page_data.get_metadatas("description")[0]
+        item["img_src"] =      page_data.get_metadatas("image")[0]
+        
+        return item
